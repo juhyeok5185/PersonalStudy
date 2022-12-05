1c2539628de9c76ed656ebcf16899423ef493c32
juhyeok5185 <117324639+juhyeok5185@users.noreply.github.com>
2022-12-05 20:57:38 +0900

    DreamCoding_JS

---
+++ JavaScript/variable.js
@@ -0,0 +1,12 @@
+// 1. use trict
+// 명확한 조건의 코딩을 하기위해 선언한다.
+'use strict'; 
+
+// 2. Variable
+// let (ES6)
+{
+    let name = 'Danny';
+    console.log(name);
+    name = 'hello';
+    console.log(name);
+}
