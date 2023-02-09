47cf79644b71bbfff97dcec0936da0f6acdf43f2
juhyeok5185 <117324639+juhyeok5185@users.noreply.github.com>
2023-02-09 19:09:32 +0900

    added

--- Todo/JavaScript/script.js
+++ Todo/JavaScript/script.js
@@ -1,14 +1,23 @@
 'use strict'
 let list = [];
+let num = 0;
+
+
+
 $(document).ready(function () {
-$('#comment-submit').click(function(){
+$('#btn-submit').click(function(){
   const board = {
     content:$('#comment').val(),
     date:$('#input-date').val()
   }
   list.push(board);
   $('#comment').val(" ");
-  $('#content').text(list);
+  $('#tbody').append("<tr>");
+  $('#tbody').append("<td><a href='#' id='textdeco'>"+ list[num].content +"</a></td>");
+  $('#tbody').append("<td>"+ list[num].date +"</td>");
+  $('#tbody').append("<td> <input class='form-check-input' type='checkbox' id='check'></td>");
+  $('#tbody').append("</tr>");
+  num++;
 })
 
 
