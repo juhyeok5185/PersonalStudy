f5d6e33c91bd4f86eb372977d1b308bbfaa07466
juhyeok5185 <117324639+juhyeok5185@users.noreply.github.com>
2023-02-16 16:47:42 +0900

    added

---
+++ Spring/HelloSpring/src/main/java/Hello/HelloSpring/domain/Member.java
@@ -0,0 +1,23 @@
+package Hello.HelloSpring.domain;
+
+public class Member {
+    //ID는 시스템이 저장하는 ID를 말한다.
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
